ffd983cd1dc516beb91b16a781525095346c5779
jykang24 <ttochi0105@naver.com>
2025-02-11 15:32:35 +0900

    fix: Hash password when insert data to DB, Add searchUser

--- actions/sign.ts
+++ actions/sign.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import { signIn, signOut } from '@/lib/auth';
 import prisma from '@/lib/db';
 
@@ -17,6 +17,10 @@ export const logout = async () => {
 export async function hashPassword(passwd: string) {
   return hash(passwd, 10);
 }
+// 비밀번호 비교 (해시값 검증)
+// export async function comparePassword(passwd: string, hashedPasswd: string) {
+//   return await compare(passwd, hashedPasswd);
+// }
 export const myLogin = async ({ email, password }: Props) => {
   try {
     const result = await signIn('credentials', {
@@ -37,22 +41,49 @@ export const insertUser = async ({
   email,
   nickname,
   passwd,
+  provider,
 }: {
   email: string;
   nickname: string;
   passwd: string;
+  provider: string;
 }) => {
   try {
+    //passwd 암호화
+    passwd = await hashPassword(passwd);
     const user = await prisma.user.create({
       data: {
         email,
         nickname,
         passwd,
+        provider,
       },
     });
     console.log('insert user into User >>', user);
+    return true;
   } catch (err) {
     console.log('insertUser error:', err);
+    return false;
+  }
+};
+//아이디로 회원정보 존재여부 검색
+export const searchUser = async ({ email }: { email: string }) => {
+  try {
+    const existUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existUser) {
+      console.log('searchUser - existUser :', existUser);
+      return true;
+    } else {
+      console.log('searchUser - no exist User');
+      return false;
+    }
+  } catch (err) {
+    console.log('searchUser Error', err);
+    return false;
   }
 };
 
@@ -64,10 +95,17 @@ export const getUser = async ({ email, password }: Props) => {
       },
     }); //유저 존재하는지 체크
 
-    if (!user || user.passwd !== password) {
-      //비밀번호 검증
+    if (!user) {
       console.log('아이디 또는 비밀번호가 틀렸습니다.');
-      return null;
+      return false;
+    }
+    //비밀번호 검증
+    if (user && user.passwd) {
+      const res = await compare(password, user.passwd);
+      if (!res) {
+        console.log('아이디 또는 비밀번호가 틀렸습니다.');
+        return false;
+      }
     }
     console.log('getUser from DB >>', user);
     return user;
