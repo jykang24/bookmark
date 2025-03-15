5de0cf6c4b62330d3df87efa4d20159fe4b7a553
jykang24 <ttochi0105@naver.com>
2025-03-15 13:23:17 +0900

    feat: Set dark class to html

--- components/DarkmodeBtn.tsx
+++ components/DarkmodeBtn.tsx
@@ -2,9 +2,19 @@
 
 import useThemeStore from '@/app/store';
 import { Moon, Sun } from 'lucide-react';
+import { useEffect } from 'react';
 
 export default function DarkmodeBtn() {
   const { isDarkMode, toggleDarkMode } = useThemeStore();
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
   return (
     <button onClick={toggleDarkMode}>{isDarkMode ? <Moon /> : <Sun />}</button>
   );
